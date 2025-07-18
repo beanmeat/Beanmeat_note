7f14330054b5d79065b4f258ebc4ebc597bf7d0c
tianchenhui <3090018282@qq.com>
2025-07-18 19:13:37 +0800

    react_router

---
+++ Front/React/react_staging/10_精准匹配与模糊匹配/src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import {Switch,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Test from './pages/Test'
+import Header from './components/Header'
+import BeanmeatNavLink from "./components/BeanmeatNavLink";
+import './App.css'
+
+export default class App extends Component {
+
+    render() {
+
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-xs-offset-2 col-xs-8">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-2 col-xs-offset-2">
+                        <div className="list-group">
+                            <BeanmeatNavLink to='/about'>About</BeanmeatNavLink>
+                            <BeanmeatNavLink to='/home'>Home</BeanmeatNavLink>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="panel">
+                            <div className="panel-body">
+                                <Switch>
+                                    {/*严格匹配*/}
+                                    <Route exact={true} path='/about' component={About} />
+                                    <Route exact={true} path='/home' component={Home} />
+                                </Switch>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
